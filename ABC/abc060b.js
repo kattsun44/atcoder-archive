d7a8790736f40325e0f825a725b6d59fb2787e8c
kattsun <08063320793a11@gmail.com>
2020-12-18 08:26:29 +0900

    B - Choose Integers解けた

--- ABC/abc060b.js
+++ ABC/abc060b.js
@@ -1,16 +1,16 @@
 function Main(input) {
     const [a, b, c] = input.split(' ').map((a, b) => Number(a, b));
     let t = 0;
+    let ans = 'NO'
 
-    for (let i = 1; i <= 100000; i++) {
+    for (let i = 1; i <= 100; i++) {
         t = a * i % b
         if (t === c) {
-            console.log('YES');
+            ans = 'YES'
             break;
-        } else if (i === 100000) {
-            console.log('NO');
         }
     }
+    console.log(ans);
 }
 
 Main(require('fs').readFileSync('/dev/stdin', 'utf8'));
