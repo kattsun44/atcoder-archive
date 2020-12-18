871e543574b8a4e8ad97c3a0620270cd8480861a
kattsun <08063320793a11@gmail.com>
2020-12-18 15:47:55 +0900

    B - Trained?を連想配列になおしてクリア

--- ABC/abc065b.js
+++ ABC/abc065b.js
@@ -1,23 +1,23 @@
 function Main(input) {
     input = input.split('\n').map((a, b) => Number(a,b));
     const n = input[0];
-    const log = [1];
+    const log = {};
     let count = 0;
 
     for (let i = 1; i <= n;) {
         count++;
-        a = input[i];
+        const a = input[i];
 
         // ボタンの数字が「2」の場合、カウント回数を出力
         if (a === 2) {
             console.log(count);
             break;
-        // a=1 or logに今まで押したボタンの番号があればbreak、なければlogに追加
-        } else if (a === 1 || log.indexOf(a) !== -1) {
+        // logに今まで押したボタンの番号があれば-1を出力しbreak, なければlogに追加
+        } else if (i in log) {
             console.log(-1);
             break;
         } else {
-            log.push(a);
+            log[i] = a;
         }
         // 次に押すボタンはa番目
         i = a;
