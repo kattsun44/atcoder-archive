8cb276077fe902b5fbd51879128a5f3154b7fba1
kattsun <08063320793a11@gmail.com>
2020-12-20 14:23:24 +0900

    abc182をPythonでbまでクリア

---
+++ ABC/abc182/abc182a.py
@@ -0,0 +1,7 @@
+def main():
+    x = list(map(int, input().split(' ')))
+    a, b = x
+    print(2 * a + 100 - b)
+
+if __name__ == '__main__':
+    main()
---
+++ ABC/abc182/abc182b.py
@@ -0,0 +1,19 @@
+import statistics as stat
+
+def main():
+    x = input().split('\n')
+    n = int(x[0])
+    nums = list(map(int, input().split(' ')))
+    cnt = 0
+    gcd = []
+
+    for i in range(max(nums) - 1):
+        for num in nums:
+            if num % (i + 2) == 0:
+                cnt+=1
+                gcd.append(i + 2)
+        cnt = 0
+    print(stat.mode(gcd))
+
+if __name__ == '__main__':
+    main()
