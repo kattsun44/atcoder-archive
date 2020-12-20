7f9f7da3c64de6bbf84527cfc51b28a6a9ce4541
kattsun <08063320793a11@gmail.com>
2020-12-20 15:04:21 +0900

    Unused variable 'i'と出るため'i' -> '_'に変更

--- ABC/abc181/abc181b.py
+++ ABC/abc181/abc181b.py
@@ -1,7 +1,7 @@
 def main():
     n = int(input())
     total = 0
-    for i in range(n):
+    for _ in range(n):
         a, b  = list(map(int, input().split(' ')))
         total += (b - a + 1) * (a + b) / 2
     print(int(total))
