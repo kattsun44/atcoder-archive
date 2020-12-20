c5cbced97dbee319354f0e0f25e3da90471c4727
kattsun <08063320793a11@gmail.com>
2020-12-20 21:58:14 +0900

    abc177 clear

--- ABC/abc177/abc177a.py
+++ ABC/abc177/abc177a.py
@@ -1,5 +1,9 @@
 def main():
-    x = input()
+    d, t, s = list(map(int, input().split(' '))) 
+    if d / s <= t:
+        print('Yes')
+    else:
+        print('No')
 
 if __name__ == '__main__':
     main()
--- ABC/abc177/abc177b.py
+++ ABC/abc177/abc177b.py
@@ -1,5 +1,20 @@
 def main():
-    x = input()
+    s = input()
+    t = input()
+    cnt = 0
+    rep = len(t)
+    
+
+    for i in range(len(s) - len(t) + 1):
+        comp = s[i:i + len(t)]
+        for j in range(len(t)):
+            if comp[j] != t[j]:
+                cnt += 1
+        if rep > cnt:
+            rep = cnt
+        cnt = 0
+    print(rep)
+
 
 if __name__ == '__main__':
     main()
