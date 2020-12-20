8912dce7e3d7dfc0cd60d91bd68b9d36bc55a7a6
kattsun <08063320793a11@gmail.com>
2020-12-20 15:28:36 +0900

    たのしい

--- ABC/abc180/abc180a.py
+++ ABC/abc180/abc180a.py
@@ -1,5 +1,6 @@
 def main():
-    x = input()
+    n, a, b = list(map(int, input().split(' ')))
+    print(n - a + b)
 
 if __name__ == '__main__':
     main()
--- ABC/abc180/abc180b.py
+++ ABC/abc180/abc180b.py
@@ -1,5 +1,13 @@
+import math
 def main():
-    x = input()
+    n = int(input())
+    ps = list(map(int, input().split(' ')))
+    m = sum(map(abs, ps))
+    e = math.sqrt(sum(map(lambda x: x ** 2,ps)))
+    c = max(map(abs, ps))
+    print(m)
+    print(e)
+    print(c)
 
 if __name__ == '__main__':
     main()
