695976307ee072014b561fb6dcde233a57342122
kattsun <08063320793a11@gmail.com>
2020-12-21 08:51:43 +0900

    abc175b not clear

--- ABC/abc175/abc175b.py
+++ ABC/abc175/abc175b.py
@@ -1,7 +1,27 @@
 def main():
-    n = input()
+    n = int(input())
+    l = list(map(int, input().split(' ')))
+    cnt = 0
     
-    print(n)
+    for i in range(len(l)):
+        l1 = l[0:i] + l[i + 1:]
+        for j in range(len(l1)):
+            l2 = l1[0:j] + l1[j + 1:]
+            for k in range(len(l2)):
+                es = [l[i], l1[j], l2[k]]
+                print(es, max(es), sum(es))
+                if len(es) == len(set(es)):
+                    if (max(es) * 2 < sum(es)):
+                        cnt += 1
+            l1 = l1[1:]
+            if len(l1) <= 3:
+                break
+        l = l[1:]
+        if len(l) <= 3:
+            break
+    
+    print(cnt)
+
 
 if __name__ == '__main__':
     main()
--- ABC/abc176/abc176b.py
+++ ABC/abc176/abc176b.py
@@ -1,5 +1,7 @@
 def main():
-    x = input()
+    n = list(map(int, list(input())))
+    t = sum(n)
+    print('Yes' if t % 9 == 0 else 'No')
 
 if __name__ == '__main__':
     main()
