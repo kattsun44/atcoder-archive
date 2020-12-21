c7e75e9b43888c91c7e4c670e98a24c00c059839
kattsun <08063320793a11@gmail.com>
2020-12-22 07:47:08 +0900

    abc175b clear

--- ABC/abc175/abc175b.py
+++ ABC/abc175/abc175b.py
@@ -4,21 +4,12 @@ def main():
     cnt = 0
     
     for i in range(len(l)):
-        l1 = l[0:i] + l[i + 1:]
-        for j in range(len(l1)):
-            l2 = l1[0:j] + l1[j + 1:]
-            for k in range(len(l2)):
-                es = [l[i], l1[j], l2[k]]
-                print(es, max(es), sum(es))
+        for j in range(i):
+            for k in range(j):
+                es = [l[i], l[j], l[k]] 
                 if len(es) == len(set(es)):
                     if (max(es) * 2 < sum(es)):
                         cnt += 1
-            l1 = l1[1:]
-            if len(l1) <= 3:
-                break
-        l = l[1:]
-        if len(l) <= 3:
-            break
     
     print(cnt)
 
