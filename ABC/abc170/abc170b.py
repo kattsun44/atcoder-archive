a22c959bfb1de1a7a80aa9c63061bfa725141688
kattsun <08063320793a11@gmail.com>
2020-12-23 07:39:39 +0900

    abc170b clear!

--- ABC/abc170/abc170b.py
+++ ABC/abc170/abc170b.py
@@ -1,7 +1,13 @@
 def main():
-    n = input()
+    x, y = list(map(int, input().split(' ')))
     
-    print(n)
+    for i in range(x + 1):
+        foots = 2 * i + 4 * (x - i)
+        if foots == y:
+            print('Yes')
+            break
+        elif i == x:
+            print('No')
 
 if __name__ == '__main__':
     main()
