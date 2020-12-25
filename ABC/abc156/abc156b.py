5f3c25a13bfb1a29a418fe42cf6fc1310e35de13
kattsun <08063320793a11@gmail.com>
2020-12-26 08:31:07 +0900

    abc156 b clear!

--- ABC/abc156/abc156b.py
+++ ABC/abc156/abc156b.py
@@ -1,7 +1,14 @@
 def main():
-    n = input()
+    n, k = list(map(int, input().split(' ')))
+    cnt = 1
+
+    while (n >   1):
+        n = n / k
+        if n >= 1:
+            cnt += 1
+        pass
     
-    print(n)
+    print(cnt)
 
 if __name__ == '__main__':
     main()
