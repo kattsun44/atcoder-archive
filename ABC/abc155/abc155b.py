830da0370004e2d9df60e8b91ee7f68f3408abf6
kattsun <08063320793a11@gmail.com>
2020-12-26 08:44:57 +0900

    abc155 b clear!

--- ABC/abc155/abc155b.py
+++ ABC/abc155/abc155b.py
@@ -1,7 +1,13 @@
 def main():
-    n = input()
+    n = int(input())
+    l = list(map(int, input().split(' ')))
     
-    print(n)
+    for i in range(n):
+        if l[i] % 2 == 0:
+            if l[i] % 3 != 0 and l[i] % 5 != 0:
+                print('DENIED')
+                return
+    print('APPROVED')
 
 if __name__ == '__main__':
     main()
