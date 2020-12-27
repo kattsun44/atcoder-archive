e97539c961ee4fe0171bc7c8a6ae1920e9192b52
kattsun <08063320793a11@gmail.com>
2020-12-27 20:38:30 +0900

    abc122 a clear!

--- ABC/121/a.py
+++ ABC/121/a.py
@@ -1,8 +1,13 @@
 def main():
-    n = input()
-    n = list(map(int, input().split(' ')))
-    
-    print(n)
+    n = input().strip()
+    if n == 'A':
+        print('T')
+    if n == 'T':
+        print('A')
+    if n == 'C':
+        print('G')
+    if n == 'G':
+        print('C')
 
 if __name__ == '__main__':
     main()
