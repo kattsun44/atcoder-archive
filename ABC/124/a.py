2821934e4068e742c0b11ad94de8a8c3998fe5f9
kattsun <08063320793a11@gmail.com>
2020-12-27 19:50:56 +0900

    abc124 ab clear!

--- ABC/124/a.py
+++ ABC/124/a.py
@@ -1,7 +1,12 @@
 def main():
-    n = input()
+    l = list(map(int, input().split(' ')))
+    t = 0
+
+    for i in range(2):
+        t += max(l)
+        l[l.index(max(l))] -= 1
     
-    print(n)
+    print(t)
 
 if __name__ == '__main__':
     main()
--- ABC/124/b.py
+++ ABC/124/b.py
@@ -1,7 +1,14 @@
 def main():
-    n = input()
+    n = int(input())
+    H = list(map(int, input().split(' ')))
+    cnt = 0
+    pre = H[0]
     
-    print(n)
+    for i in range(n):
+        if H[i] >= pre:
+            cnt += 1
+            pre = H[i]
+    print(cnt)
 
 if __name__ == '__main__':
     main()
