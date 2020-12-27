dfc2c7dc8c1fe2316bc6f5dfeec15bdab4b70020
kattsun <08063320793a11@gmail.com>
2020-12-27 10:24:48 +0900

    abc139 ab clear!

--- ABC/139/a.py
+++ ABC/139/a.py
@@ -1,7 +1,12 @@
 def main():
-    n = input()
+    s = input().strip()
+    t = input().strip()
+    cnt = 0
     
-    print(n)
+    for i in range(3):
+        if s[i] == t[i]:
+            cnt += 1
+    print(cnt)
 
 if __name__ == '__main__':
     main()
--- ABC/139/b.py
+++ ABC/139/b.py
@@ -1,7 +1,13 @@
 def main():
-    n = input()
+    a, b = list(map(int, input().split(' ')))
+    cnt, t = [0, 0]
+
+    while b != 1 and t < b:
+        cnt += 1
+        t += a - 1 if cnt != 1 else a
+        pass
     
-    print(n)
+    print(cnt)
 
 if __name__ == '__main__':
     main()
