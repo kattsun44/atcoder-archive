5408498a8692a515e14576cd723b97fd83978d33
kattsun <08063320793a11@gmail.com>
2020-12-27 14:46:13 +0900

    abc132 b clear!

--- ABC/132/b.py
+++ ABC/132/b.py
@@ -1,8 +1,14 @@
 def main():
-    n = input().strip()
-    n = list(n)
+    n = int(input())
+    P = list(map(int, input().split(' ')))
+    cnt = 0
     
-    print(n)
+    for i in range(n - 2):
+        l = [P[i], P[i + 1], P[i + 2]]
+        l.sort()
+        if l[1] == P[i + 1]:
+            cnt += 1
+    print(cnt)
 
 if __name__ == '__main__':
     main()
