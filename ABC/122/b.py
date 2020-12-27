1e50c669f1caffb52056e0017b2716a87210b3ab
kattsun <08063320793a11@gmail.com>
2020-12-28 07:07:37 +0900

    abc122 ab clear!

--- ABC/122/b.py
+++ ABC/122/b.py
@@ -1,8 +1,18 @@
 def main():
-    n = input()
-    n = list(map(int, input().split(' ')))
+    s = input().strip()
+    acgt = ['A', 'C', 'G', 'T']
+    cnt = 0
+    ans = 0
+
+    for i in s:
+        if i in acgt:
+            cnt += 1
+            if cnt > ans:
+                ans = cnt
+        else:
+            cnt = 0
     
-    print(n)
+    print(ans)
 
 if __name__ == '__main__':
     main()
