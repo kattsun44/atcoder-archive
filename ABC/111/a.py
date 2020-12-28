e955a954564d328759948733aa31a01993a1c931
kattsun <08063320793a11@gmail.com>
2020-12-28 19:02:30 +0900

    abc112 ab clear!

--- ABC/111/a.py
+++ ABC/111/a.py
@@ -1,8 +1,11 @@
 def main():
-    n = input()
-    n = list(map(int, input().split(' ')))
+    n = input().strip()
+    ans = ''
+
+    for i in n:
+        ans += '9' if i == '1' else '1'
     
-    print(n)
+    print(int(ans))
 
 if __name__ == '__main__':
     main()
--- ABC/111/b.py
+++ ABC/111/b.py
@@ -1,7 +1,7 @@
+import math
 def main():
-    n = input()
-    n = list(map(int, input().split(' ')))
-    
+    n = int(input())
+    n = math.ceil(n / 111) * 111
     print(n)
 
 if __name__ == '__main__':
