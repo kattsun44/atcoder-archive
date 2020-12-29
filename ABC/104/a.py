c4924b79803e29a28118ad30ad00ce3ece3cce56
kattsun <08063320793a11@gmail.com>
2020-12-29 15:14:13 +0900

    abc104 ab clear!

--- ABC/104/a.py
+++ ABC/104/a.py
@@ -1,8 +1,12 @@
 def main():
-    n = input()
-    n = list(map(int, input().split(' ')))
-    
-    print(n)
+    r = int(input())
 
+    if r < 1200:
+        print('ABC')
+    elif r < 2800:
+        print('ARC')
+    else:
+        print('AGC')
+    
 if __name__ == '__main__':
     main()
