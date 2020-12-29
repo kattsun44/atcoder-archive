9df4f1e788ee4b734c61c75867149d327859809d
kattsun <08063320793a11@gmail.com>
2020-12-29 13:10:35 +0900

    abc107 ab clear!

--- ABC/107/a.py
+++ ABC/107/a.py
@@ -1,8 +1,7 @@
 def main():
-    n = input()
-    n = list(map(int, input().split(' ')))
+    n, i = list(map(int, input().split(' ')))
     
-    print(n)
+    print(n - i + 1)
 
 if __name__ == '__main__':
     main()
--- ABC/107/b.py
+++ ABC/107/b.py
@@ -1,8 +1,33 @@
 def main():
-    n = input()
-    n = list(map(int, input().split(' ')))
+    h, w = list(map(int, input().split(' ')))
+    B = []
+    hc = {} # 行方向のカウンタ
+    wc = {} # 列方向のカウンタ
+
+    for i in range(h):
+        hc[i] = 0
+    for i in range(w):
+        wc[i] = 0
     
-    print(n)
+    for i in range(h):
+        r = input().strip()
+        B.append(r)
+
+        # 白「.」マスをカウント
+        for j in range(w):
+            if r[j] == '.':
+                hc[i] += 1
+                wc[j] += 1
+
+    for i in range(h):
+        # 白マスの数が列数以下の行のみ
+        if hc[i] != w:
+            output = ''
+            for j in range(w):
+                # 白マスの数が行数以下の列をアウトプットに代入する
+                if wc[j] != h:
+                    output += B[i][j]
+            print(output)
 
 if __name__ == '__main__':
     main()
