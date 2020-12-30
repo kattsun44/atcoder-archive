78639cb2503351dac789efe9c91a79b5d91df90c
kattsun <08063320793a11@gmail.com>
2020-12-30 17:59:51 +0900

    abc93 ab clear!

--- ABC/93/a.py
+++ ABC/93/a.py
@@ -1,8 +1,8 @@
 def main():
-    n = input()
-    n = list(map(int, input().split(' ')))
-    
-    print(n)
+    n = set(input().strip())
+    s = set(['a', 'b', 'c'])
+    ans = 'Yes' if n == s else 'No'
+    print(ans)
 
 if __name__ == '__main__':
     main()
