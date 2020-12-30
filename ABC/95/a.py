4e6af25bbc7042658ab83dc72023cd0402ecbe55
kattsun <08063320793a11@gmail.com>
2020-12-30 16:31:01 +0900

    abc95 ab clear!

--- ABC/95/a.py
+++ ABC/95/a.py
@@ -1,8 +1,7 @@
 def main():
-    n = input()
-    n = list(map(int, input().split(' ')))
+    s = list(input().strip())
     
-    print(n)
+    print(700 + 100 * s.count('o'))
 
 if __name__ == '__main__':
     main()
--- ABC/95/b.py
+++ ABC/95/b.py
@@ -1,8 +1,12 @@
+import math
 def main():
-    n = input()
-    n = list(map(int, input().split(' ')))
+    n, x = list(map(int, input().split(' ')))
+    L = []
+
+    for i in range(n):
+        L.append(int(input()))
     
-    print(n)
+    print(n + math.floor(((x - sum(L)) / min(L))))
 
 if __name__ == '__main__':
     main()
