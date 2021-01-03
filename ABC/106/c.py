675ed7bcf8e2f40b479bd92015d80b1bdc6bb73a
kattsun <08063320793a11@gmail.com>
2021-01-03 13:54:13 +0900

    not clear abc170 c

--- ABC/106/c.py
+++ ABC/106/c.py
@@ -1,8 +1,13 @@
 def main():
-    n = input()
-    n = list(map(int, input().split(' ')))
+    S = list(map(int,list(input().strip())))
+    K = int(input())
+    t = 0
     
-    print(n)
+    for i in S:
+        t += i ** 10000
+        if t > K:
+            print(i)
+            return
 
 if __name__ == '__main__':
     main()
