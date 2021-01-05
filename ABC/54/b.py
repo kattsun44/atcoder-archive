5e7f614a511a956fe8a3dd2b042530d743c67d89
kattsun <08063320793a11@gmail.com>
2021-01-06 08:29:02 +0900

    clear abc54b

--- ABC/54/b.py
+++ ABC/54/b.py
@@ -1,8 +1,28 @@
 def main():
-    n = input()
-    n = list(map(int, input().split(' ')))
-    
-    print(n)
+    n, m = list(map(int, input().split(' ')))
+    A = []
+    B = []
+    cnt = 0
+
+    for _ in range(n):
+        A.append(input().strip())
+    for _ in range(m):
+        B.append(input().strip())
+
+    # 画像Aの中で画像Bを縦横にn-m+1回動かして比較
+    for i in range(n - m + 1):
+        for j in range(n - m + 1):
+            if A[i][j] == B[0][0]:
+                for k in range(m):
+                    for l in range(m):
+                        if A[i + k][j + l] ==B[k][l]:
+                            cnt += 1
+                    if cnt == m ** 2:
+                        print('Yes')
+                        return
+                cnt = 0
+
+    print('No')
 
 if __name__ == '__main__':
     main()
