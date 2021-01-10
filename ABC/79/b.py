e1bcdd1568b909914e4d235104e3636b13037b7a
kattsun <08063320793a11@gmail.com>
2021-01-10 11:24:15 +0900

    clear abc079b

--- ABC/79/b.py
+++ ABC/79/b.py
@@ -1,8 +1,16 @@
 def main():
-    n = input()
-    n = list(map(int, input().split(' ')))
+    # n番目のリュカ数を求める
+    n = int(input())
+    L0 = 2
+    L1 = 1
+    ans = L1
+
+    for i in range(n - 1):
+        ans = L0 + L1
+        L0 = L1
+        L1 = ans
     
-    print(n)
+    print(ans)
 
 if __name__ == '__main__':
     main()
