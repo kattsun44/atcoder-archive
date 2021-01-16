332889a7ce311a213fbcb02ff6dcee53536acb9a
kattsun <08063320793a11@gmail.com>
2021-01-16 21:42:28 +0900

    learn C++

---
+++ ABC/76/a/a.cpp
@@ -0,0 +1,19 @@
+/**
+*    author:  kattsun
+*    created: 16-01-2021 20:20:45
+**/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+   cin.tie(0);
+   ios_base::sync_with_stdio(false);
+   int r, g;
+
+   cin >> r >> g;
+   cout << g * 2 - r << endl;
+
+   return 0;
+}
