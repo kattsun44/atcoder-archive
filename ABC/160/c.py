d39b80c9e0a5c6c392e5e718ec9a25707cb828f6
kattsun <08063320793a11@gmail.com>
2021-01-21 07:45:27 +0900

    clear abc160

--- ABC/160/c.py
+++ ABC/160/c.py
@@ -1,8 +1,15 @@
 def main():
-    n = input()
-    n = list(map(int, input().split(' ')))
+    k, n = map(int, input().split(' '))
+    a = list(map(int, input().split(' ')))
+    a.append(k + a[0])
     
-    print(n)
+    # 2点間距離のリスト
+    d = []
+
+    for i in range(n):
+        d.append(abs(a[i] - a[i+1]))
+    d.remove(max(d))
+    print(sum(d))
 
 if __name__ == '__main__':
     main()
