aab68186f56ca1f90440dcabf9242d20a710529b
kattsun <08063320793a11@gmail.com>
2021-01-23 23:31:52 +0900

    contest abc189

---
+++ ABC/189/d/d.py
@@ -0,0 +1,16 @@
+"""
+*    author:  kattsun
+*    created: 23-01-2021 22:18:41
+"""
+
+def main():
+    n = int(input())
+    
+
+    for i in range(n):
+        s
+    
+    print(n)
+
+if __name__ == '__main__':
+    main()
