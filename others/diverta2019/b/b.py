91d657035f3e5f94f3305c60b5e3c31ff2ad4d05
kattsun <08063320793a11@gmail.com>
2021-03-03 07:53:22 +0900

    make forder "others"

---
+++ others/diverta2019/b/b.py
@@ -0,0 +1,13 @@
+"""
+*    author:  kattsun
+*    created: 03-03-2021 07:52:54
+"""
+
+def main():
+    n = int(input())
+    s = input().strip()
+    n = map(int, input().split())
+    a = list(map(int, input().split()))
+
+if __name__ == '__main__':
+    main()
