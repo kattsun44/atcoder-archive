7dc88edcda7d6244f81b6702e30292826bb2fd03
kattsun44 <08063320793a11@gmail.com>
2021-03-14 11:07:48 +0900

    abc195

--- ABC/abc195/b/b.py
+++ ABC/abc195/b/b.py
@@ -1,31 +1,24 @@
 """
 *    author:  kattsun
-*    created: 13-03-2021 21:01:54
+*    created: 14-03-2021 10:34:13
 """
 
 def main():
     a,b,w = map(int, input().split())
-    inf = float('inf')
-    mn = inf
-    mx = -1
-    cnt = 0
-    jdg = False
-    for i in range(a,b):
-        c = w*1000//i # 軽い方の最大個数
-        for j in range(c):
-            cnt += j
-            r = w*1000 - i*j
-            cnt += round(r/b)
-            mn = min(cnt,mn)
-            mx = max(cnt,mx)
-            cnt = 0
-        if w*1000 - (i * j + round(r/b)*b) == 0:
-            jdg = True
+    w *= 1000
+    INF = float('inf')
+    l = INF
+    r = -INF
+
+    for n in range(1,w+1):
+        if a*n <= w and b*n >=w:
+            l = min(l,n)
+            r = max(r,n)
     
-    if jdg:
-        print(mn,mx)
-    else:
+    if l == INF:
         print('UNSATISFIABLE')
+    else:
+        print(l,r)
 
 if __name__ == '__main__':
     main()
