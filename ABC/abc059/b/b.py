2519f5a98530f9576a4876cb5e229bc544da7bec
kattsun44 <08063320793a11@gmail.com>
2021-03-26 22:52:00 +0900

    clear abc059b

---
+++ ABC/abc059/b/b.py
@@ -0,0 +1,20 @@
+"""
+*    author:  kattsun
+*    created: 26-03-2021 22:49:04
+"""
+
+
+def main():
+    a = int(input())
+    b = int(input())
+
+    if a > b:
+        print('GREATER')
+    elif a < b:
+        print('LESS')
+    else:
+        print('EQUAL')
+
+
+if __name__ == '__main__':
+    main()
