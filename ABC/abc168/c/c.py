c0a8ed405b1be8ef4e45ebd1c127910907a363df
kattsun44 <08063320793a11@gmail.com>
2021-03-31 20:42:34 +0900

    20210331

--- ABC/abc168/c/c.py
+++ ABC/abc168/c/c.py
@@ -1,8 +1,19 @@
+"""
+*    author:  kattsun
+*    created: 31-03-2021 18:53:54
+"""
+from math import cos, sqrt, radians
+
+
 def main():
-    n = input()
-    n = list(map(int, input().split(' ')))
-    
-    print(n)
+    A, B, H, M = map(int, input().split())
+    h = (H * 30 + M * 0.5)
+    m = M * 6
+    theta = abs(h - m)
+    ans = sqrt(A ** 2 + B ** 2 - 2 * A * B * cos(radians(theta)))
+
+    print(ans)
+
 
 if __name__ == '__main__':
     main()
