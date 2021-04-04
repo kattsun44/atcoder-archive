27bec5a33dd27ce6328c2704305682539afc8410
kattsun44 <08063320793a11@gmail.com>
2021-04-04 21:53:47 +0900

    not clear keyence2021a

---
+++ others/keyence2021/a/a.py
@@ -0,0 +1,14 @@
+"""
+*    author:  kattsun
+*    created: 04-04-2021 21:41:04
+"""
+
+
+def main():
+    n = int(input())
+    a = list(map(int, input().split()))
+    b = list(map(int, input().split()))
+
+
+if __name__ == '__main__':
+    main()
