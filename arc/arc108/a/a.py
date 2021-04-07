38bf800573e1ee22b0f20877b59f50029eab4b63
kattsun44 <08063320793a11@gmail.com>
2021-04-07 21:13:49 +0900

    解説AC

--- arc/arc108/a/a.py
+++ arc/arc108/a/a.py
@@ -1,33 +1,13 @@
-"""
-*    author:  kattsun
-*    created: 07-04-2021 20:25:16
-"""
-from functools import reduce
-
-
-def make_divisors(n):
-    lower_divisors, upper_divisors = [], []
-    i = 1
-    while i * i <= n:
-        if n % i == 0:
-            lower_divisors.append(i)
-            if i != n // i:
-                upper_divisors.append(n // i)
-        i += 1
-    return lower_divisors + upper_divisors[::-1] + [1]
-
-
 def main():
     S, P = map(int, input().split())
-    divList = make_divisors(P)
-    print(divList)
+    res = []
+    for i in range(1, int(P ** 0.5) + 1):
+        if P % i == 0:
+            res.append(i)
+            res.append(P // i)
 
-    for i in range(len(divList) - 1):
-        # 総乗
-        print(divList[:i + 1], divList[i + 1:])
-        N = reduce(lambda x, y: x * y, divList[:i + 1])
-        M = reduce(lambda x, y: x * y, divList[i + 1:])
-        if N + M == S and N * M == P:
+    for x in res:
+        if x + P // x == S:
             print('Yes')
             return
     print('No')
