f095c7e426328e810be1983b2a0942bfa62f57ab
kattsun44 <08063320793a11@gmail.com>
2021-05-02 23:48:07 +0900

    clear

---
+++ abc/abc196/a/s.py
@@ -0,0 +1,15 @@
+"""
+*    author:  kattsun
+*    created: 02-05-2021 23:46:16
+"""
+
+
+def main():
+    a, b = map(int, input().split())
+    c, d = map(int, input().split())
+
+    print(b - c)
+
+
+if __name__ == '__main__':
+    main()
