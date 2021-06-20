2a4fa6df8968d588e8468e4423476c30e4381925
kattsun <08063320793a11@gmail.com>
2021-06-21 07:42:10 +0900

    典型90問解答スタート’

---
+++ others/typical90/001/main.py
@@ -0,0 +1,12 @@
+"""
+*    author:  kattsun
+*    created: 21-06-2021 07:12:03
+"""
+
+def main():
+    N, L = map(int, input().split())
+    K = int(input())
+    A = list(map(int, input().split()))
+
+if __name__ == '__main__':
+    main()
