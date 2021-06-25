d1c39cbdee6a5774139ff600e57d02fba35eb89b
kattsun44 <08063320793a11@gmail.com>
2021-06-25 13:16:49 +0900

    解説AC

--- arc/arc110/a/main.py
+++ arc/arc110/a/main.py
@@ -2,10 +2,22 @@
 *    author:  kattsun
 *    created: 24-06-2021 23:36:53
 """
+import math
+from functools import reduce
+
+
+def my_lcm_base(x, y):
+    return (x * y) // math.gcd(x, y)
+
+
+def my_lcm(*numbers):
+    return reduce(my_lcm_base, numbers, 1)
 
 
 def main():
     N = int(input())
+    ans = my_lcm(*[i for i in range(2, N+1)]) + 1
+    print(ans)
 
 
 if __name__ == '__main__':
