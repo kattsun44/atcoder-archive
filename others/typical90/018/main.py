655f2e0259f738d7aaaa5921cbeb6efd058683e7
kattsun <08063320793a11@gmail.com>
2021-07-12 08:41:26 +0900

    準備

---
+++ others/typical90/018/main.py
@@ -0,0 +1,13 @@
+"""
+*    author:  kattsun
+*    created: 12-07-2021 08:29:14
+"""
+
+def main():
+    N = int(input())
+    S = input().strip()
+    N, M = map(int, input().split())
+    A = list(map(int, input().split()))
+
+if __name__ == '__main__':
+    main()
