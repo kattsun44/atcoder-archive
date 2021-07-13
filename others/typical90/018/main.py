b886e821cac629f3408e8c001a4e1aad59883a84
kattsun44 <08063320793a11@gmail.com>
2021-07-13 23:05:22 +0900

    解説AC

--- others/typical90/018/main.py
+++ others/typical90/018/main.py
@@ -1,13 +1,23 @@
 """
 *    author:  kattsun
-*    created: 12-07-2021 08:29:14
+*    created: 13-07-2021 22:15:54
 """
+import math
+
 
 def main():
-    N = int(input())
-    S = input().strip()
-    N, M = map(int, input().split())
-    A = list(map(int, input().split()))
+    T = int(input())
+    L, X, Y = map(int, input().split())
+    Q = int(input())
+    for i in range(Q):
+        E = int(input())
+        x = 0
+        y = -L/2 * math.sin(math.radians(360 * E/T))
+        z = L/2 - L/2 * math.cos(math.radians(360 * E/T))
+        A = math.sqrt(X**2 + (y-Y)**2)
+        B = z
+        print(math.degrees(math.atan2(B, A)))
+
 
 if __name__ == '__main__':
     main()
