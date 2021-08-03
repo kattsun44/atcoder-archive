3190328ef948f11063f14928b9bed4c8ed68901d
kattsun <08063320793a11@gmail.com>
2021-08-03 17:31:24 +0900

    解説AC

--- others/typical90/082/main.py
+++ others/typical90/082/main.py
@@ -3,30 +3,25 @@
 *    created: 30-07-2021 08:39:27
 """
 
+
 def main():
     L, R = map(int, input().split())
     MOD = 10**9+7
-
-    # Lの位に等しくなるまでxを初期化
-    x = 1
-    while x*10 < L:
-        x *= 10
-
     ans = 0
-    L1 = L % MOD
-    R1 = min(x*10-1, R) % MOD
-    while x <= R:
+    for digit in range(1, 20):
+        # 今の桁での最小・最大値
+        low = 10**(digit - 1)
+        high = 10**digit - 1
 
-        # 答えを図示したときの図形
-        rectangle = (L1 * (R1 - L1 + 1)) % MOD
-        tryangle = int((R1 - L1) * (R1 - L1 + 1) / 2) % MOD
+        l = max(L, low)
+        r = min(R, high)
 
-        ans += (rectangle + tryangle) * len(str(x))
-        ans %= MOD
-        x *= 10
-        L1 = x
-        R1 = min(x*10-1, R)
-    print(ans)
+        if l <= r:
+            # 高さl+r、幅r-l+1の長方形の面積を求め、2で割る
+            ans += (l+r) * (r-l+1) * digit // 2
+    
+    print(ans % MOD)
+
 
 if __name__ == '__main__':
     main()
