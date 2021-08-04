529a6eee17b77113bf272038dcc486046d5fae97
kattsun <08063320793a11@gmail.com>
2021-08-04 17:32:28 +0900

    途中

--- others/typical90/001/main.py
+++ others/typical90/001/main.py
@@ -1,12 +1,16 @@
 """
 *    author:  kattsun
-*    created: 21-06-2021 07:12:03
+*    created: 04-08-2021 12:54:19
 """
 
 def main():
     N, L = map(int, input().split())
     K = int(input())
     A = list(map(int, input().split()))
+
+    # 貪欲法
+    def check(M):
+        
 
 if __name__ == '__main__':
     main()
