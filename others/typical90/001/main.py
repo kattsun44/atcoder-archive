c33e816c27d6582707ddd58da29c28293e10a489
kattsun44 <08063320793a11@gmail.com>
2021-08-04 22:10:55 +0900

    解説AC

--- others/typical90/001/main.py
+++ others/typical90/001/main.py
@@ -3,6 +3,7 @@
 *    created: 04-08-2021 12:54:19
 """
 
+
 def main():
     N, L = map(int, input().split())
     K = int(input())
@@ -10,7 +11,26 @@ def main():
 
     # 貪欲法
     def check(M):
-        
+        cnt = 0
+        pre = 0
+        for a in A:
+            if a - pre >= M and L - a >= M:
+                cnt += 1
+                pre = a
+        if cnt >= K:
+            return True
+        else:
+            return False
+
+    _range = (0, L+1)
+    while _range[1] - _range[0] > 1:
+        candidate = sum(_range) // 2
+        if check(candidate):
+            _range = (candidate, _range[1])
+        else:
+            _range = (_range[0], candidate)
+    print(_range[0])
+
 
 if __name__ == '__main__':
     main()
