6af22de2176ffdca2653e594cb1a306aa43694cc
kattsun44 <08063320793a11@gmail.com>
2021-08-06 20:02:38 +0900

    clear

---
+++ abc/abc046/a/main.py
@@ -0,0 +1,13 @@
+"""
+*    author:  kattsun
+*    created: 06-08-2021 20:00:36
+"""
+
+
+def main():
+    A = list(map(int, input().split()))
+    print(len(set(A)))
+
+
+if __name__ == '__main__':
+    main()
