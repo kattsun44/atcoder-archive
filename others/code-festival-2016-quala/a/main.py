a2484babffc87ce6766a5b7ae3cb5569f0537c69
kattsun44 <08063320793a11@gmail.com>
2021-08-11 22:04:06 +0900

    clear

---
+++ others/code-festival-2016-quala/a/main.py
@@ -0,0 +1,13 @@
+"""
+*    author:  kattsun
+*    created: 11-08-2021 22:01:40
+"""
+
+
+def main():
+    S = input().strip()
+    print(S[:4] + " " + S[4:])
+
+
+if __name__ == '__main__':
+    main()
