27b9f4831db2412e27132b4eb6717ebb6569e34c
kattsun <08063320793a11@gmail.com>
2021-08-18 08:26:48 +0900

    clear

---
+++ others/code-festival-2017-quala/a/main.py
@@ -0,0 +1,14 @@
+"""
+*    author:  kattsun
+*    created: 18-08-2021 08:24:52
+"""
+
+def main():
+    S = input().strip()
+    if S[:4] == 'YAKI':
+        print('Yes')
+    else:
+        print('No')
+
+if __name__ == '__main__':
+    main()
