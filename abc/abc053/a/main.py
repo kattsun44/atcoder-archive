b270abb5ea6c1a094e704b0254b070e836843009
kattsun44 <08063320793a11@gmail.com>
2021-08-22 20:21:38 +0900

    clear

---
+++ abc/abc053/a/main.py
@@ -0,0 +1,16 @@
+"""
+*    author:  kattsun
+*    created: 22-08-2021 20:19:48
+"""
+
+
+def main():
+    x = int(input())
+    if x < 1200:
+        print('ABC')
+    else:
+        print('ARC')
+
+
+if __name__ == '__main__':
+    main()
