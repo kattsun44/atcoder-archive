4023def4d30f6b4a073e1f3bf5ea7f37a8923dcb
kattsun44 <08063320793a11@gmail.com>
2021-08-27 21:33:15 +0900

    clear

---
+++ abc/abc048/a/main.py
@@ -0,0 +1,13 @@
+"""
+*    author:  kattsun
+*    created: 27-08-2021 21:31:26
+"""
+
+
+def main():
+    _, s, _ = map(str, input().split())
+    print('A' + s[0] + 'C')
+
+
+if __name__ == '__main__':
+    main()
