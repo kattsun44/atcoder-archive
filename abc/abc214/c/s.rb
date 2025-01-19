b04771302b48aa892f3b01290876bc587bb0d411
kattsun44 <kattsun4416@gmail.com>
2025-01-19 16:36:02 +0900

    Fix abc214_c
    https://atcoder.jp/contests/abc214/submissions/61861765

--- abc/abc214/c/s.rb
+++ abc/abc214/c/s.rb
@@ -3,11 +3,14 @@ s = gets.chomp.split(" ").map(&:to_i)
 t = gets.chomp.split(" ").map(&:to_i)
 ans = [0] * n
 
+start = t.index(t.min)
+
 n.times do |i|
+  index = (start + i) % n
   if i.zero?
-    ans[i] = t[i]
+    ans[index] = t[index]
   else
-    ans[i] = [t[i], ans[i-1] + s[i-1]].min
+    ans[index] = [t[index], ans[index-1] + s[index-1]].min
   end
 end
 
