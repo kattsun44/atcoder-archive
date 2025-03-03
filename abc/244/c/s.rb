bafba12cb0a6eb191d61317a67166168d6f2ab55
kattsun44 <kattsun4416@gmail.com>
2025-03-04 08:54:01 +0900

    fix: abc244c (TLE)
    https://atcoder.jp/contests/abc244/submissions/63376430

--- abc/244/c/s.rb
+++ abc/244/c/s.rb
@@ -1,14 +1,19 @@
 n = gets.to_i
 
-nums = 1.upto(n * 2 + 1).to_a
-puts nums.delete_at(0) # 高橋君
+nums = 1.upto(n * 2 + 1).to_a.map { [_1, false] }.to_h
+puts 1
+nums[1] = true # 高橋君
 
 while
   a = gets.to_i
 
   break if a == 0
 
-  nums.delete(a) # 青木君
-  puts nums.delete_at(0) # 高橋君
+  nums[a] = true # 青木君
+
+  t = nums.select { _2 == false }.keys[0]
+  puts t
+  nums[t] = true # 高橋君
+
   STDOUT.flush
 end
