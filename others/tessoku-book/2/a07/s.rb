82a1849b4814a614d3b51ccd1de3a3efe7868aa1
kattsun44 <kattsun4416@gmail.com>
2025-03-08 16:51:42 +0900

    fix: tessoku-book/a07
    https://atcoder.jp/contests/tessoku-book/submissions/63484511

--- others/tessoku-book/2/a07/s.rb
+++ others/tessoku-book/2/a07/s.rb
@@ -1,7 +1,7 @@
 # https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_g
 # A07 - Event Attendance
-# ある会社では D 日間にわたってイベントが開催され，N 人が出席します．
-# 参加者 i は Li​ 日目から Ri​ 日目まで出席する予定です．
+# ある会社では D 日間にわたってイベントが開催され，N 人が出席します．
+# 参加者 i は Li​ 日目から Ri​ 日目まで出席する予定です．
 # 各日の出席者数を出力するプログラムを作成してください．
 
 # 制約
@@ -13,13 +13,13 @@ d = gets.to_i
 n = gets.to_i
 l, r = n.times.map { gets.chomp.split(" ").map { _1.to_i - 1 } }.transpose
 
-b = [0] * d
+b = [0] * (d+1) # 最終日まで出席する人を考慮して+1日しておく
 
 # 増減
 n.times.map do |i|
   b[l[i]] += 1
-  b[r[i]+1] -= 1
+  b[r[i]+1] -= 1 # 最終日の翌日にいなくなる
 end
 
 sum = 0
-puts b.map { sum += _1 }
+puts b[...-1].map { sum += _1 } # 最終日まで出力する
