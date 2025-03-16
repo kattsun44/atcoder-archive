1e02f6595efacd1646da549181527076c3119f76
kattsun44 <kattsun4416@gmail.com>
2025-03-16 12:42:48 +0900

    fix: abc397c
    https://atcoder.jp/contests/abc397/submissions/63862923

--- abc/397/c/s.rb
+++ abc/397/c/s.rb
@@ -1,20 +1,29 @@
 n = gets.to_i
 a = gets.chomp.split.map(&:to_i)
 
+sum_l = [0] * n
+sum_r = [0] * n
+
 x = [a, [0] * n].transpose.to_h
-x[a[0]] = 1
-y = a.tally
+y = [a, [0] * n].transpose.to_h
 
-ans = 1 + a[1..].uniq.size
-anss = [2]
-
-1.upto(n-2) do |i|
-  aa = a[i]
-  x[aa] += 1
-  y[aa] -= 1
-  ans += 1 if x[aa] == 1
-  ans -= 1 if y[aa] == 0
-  anss << ans
+s = 0
+n.times do |i|
+  if x[a[i]] == 0
+    x[a[i]] = 1
+    s += 1
+  end
+  sum_l[i] = s
 end
 
-puts anss.max
+a_rev = a.reverse
+s = 0
+n.times do |i|
+  if y[a_rev[i]] == 0
+    y[a_rev[i]] = 1
+    s += 1
+  end
+  sum_r[i] = s
+end
+
+puts [sum_l[...-1], sum_r.reverse[1..]].transpose.map(&:sum).max
