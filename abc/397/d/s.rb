fed59503956ac05f4a6abfced6992e7334b34bce
kattsun44 <kattsun4416@gmail.com>
2025-03-16 12:51:27 +0900

    fix: abc397d
    https://atcoder.jp/contests/abc397/submissions/63867286

--- abc/397/d/s.rb
+++ abc/397/d/s.rb
@@ -1,11 +1,9 @@
-require 'prime'
-
 n = gets.to_i
 
 # 二分探索で ax^2 + bx + c = 0 の解を求める
 # 解がない場合は -1 を返す
 def solve(a, b, c)
-  l, r = 0, 10 ** 6
+  l, r = 0, 10 ** 18
   while r - l > 1 do
     mid = (l + r) / 2
     if a * mid * mid + b * mid + c <= 0
@@ -14,14 +12,13 @@ def solve(a, b, c)
       r = mid
     end
   end
-  return l if a * l * l + b * l + c == 0
-  return -1
+  a * l * l + b * l + c == 0 ? l : -1
 end
 
 1.upto(10 ** 6) do |d|
+  next if n % d != 0
   m = n / d
-  # 3k^2 + 3dk + d^2 - m = 0
-  k = solve(3, 3 * d, d * d - m)
+  k = solve(3, 3 * d, d * d - m) # 3k^2 + 3dk + d^2 - m = 0
   if k > 0
     puts "#{k + d} #{k}"
     exit
