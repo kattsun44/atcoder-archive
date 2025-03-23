bd053fffa5207a479e0374c232ef44419b4ad699
kattsun44 <kattsun4416@gmail.com>
2025-03-23 09:14:05 +0900

    add: abc398d
    https://atcoder.jp/contests/abc398/submissions/64115166
    
    ref: https://atcoder.jp/contests/abc398/submissions/64100068

--- abc/398/d/s.rb
+++ abc/398/d/s.rb
@@ -1,2 +1,17 @@
 N, R, C = gets.chomp.split.map(&:to_i)
-s = gets.chomp
+S = gets.chomp.chars
+ans = []
+
+direct = { "N" => [-1, 0], "E" => [0, 1], "S" => [1, 0], "W" => [0, -1] }
+
+log = {}
+log[[R, C]] = true
+
+rc = [0, 0]
+S.each_with_index do |s, i|
+  rc = [rc, direct[s]].transpose.map(&:sum)
+  ans << (log[rc] ? 1 : 0)
+  log[[[R, C], rc].transpose.map(&:sum)] = true
+end
+
+puts ans.join
