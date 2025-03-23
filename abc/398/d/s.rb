5280023b08baedd8bbf003b19829a87fa7649ff0
kattsun44 <kattsun4416@gmail.com>
2025-03-23 19:10:37 +0900

    refactor: abc398d 可視性を上げる

--- abc/398/d/s.rb
+++ abc/398/d/s.rb
@@ -2,16 +2,26 @@ N, R, C = gets.chomp.split.map(&:to_i)
 S = gets.chomp.chars
 ans = []
 
+# 焚き火が移動する方向
 direct = { "N" => [-1, 0], "E" => [0, 1], "S" => [1, 0], "W" => [0, -1] }
 
+# 高橋君の位置ログ
 log = {}
 log[[R, C]] = true
 
-rc = [0, 0]
-S.each_with_index do |s, i|
-  rc = [rc, direct[s]].transpose.map(&:sum)
-  ans << (log[rc] ? 1 : 0)
-  log[[[R, C], rc].transpose.map(&:sum)] = true
+# 焚き火の初期位置
+r = c = 0
+
+S.each do |s|
+  # 焚き火の位置の更新
+  dr, dc = direct[s]
+  r += dr
+  c += dc
+
+  # Q: なぜ更新後の焚き火の位置が高橋くんの過去いた位置にくると 1 になる？
+  ans << (log[[r, c]] ? 1 : 0)
+
+  log[[R + r, C + c]] = true
 end
 
 puts ans.join
